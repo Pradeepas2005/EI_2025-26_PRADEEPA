490e194f758360d00bc8e40c7004cc2ba2f8b0cb
Pradeepas2005 <pradeeparathika@gmail.com>
2025-10-01 18:04:40 +0530

    Update FormHistory.java

--- Exercise 1/Behavioural Patterns/Memento Pattern/Form Filling/FormHistory.java
+++ Exercise 1/Behavioural Patterns/Memento Pattern/Form Filling/FormHistory.java
@@ -1,16 +1,19 @@
 import java.util.Stack;
 
-class FormHistory {
-    private Stack<FormState> history = new Stack<>();
+public class FormHistory {
+    private Stack<FormState> history = new Stack<FormState>();
 
     public void push(FormState state) {
         history.push(state);
     }
 
     public FormState pop() {
-        if (!history.isEmpty()) {
+        if (!history.isEmpty())
             return history.pop();
-        }
         return null;
+    }
+
+    public boolean isEmpty() {
+        return history.isEmpty();
     }
 }
